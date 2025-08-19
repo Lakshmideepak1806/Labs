80a39bc757e7576cc0b3c025843b3c5cb62f159a
Deepak <lakshmideepak1806@gmail.com>
2025-08-19 13:33:59 +0530

    message

--- src/test/java/selenium_testng/Lab9_4.java
+++ src/test/java/selenium_testng/Lab9_4.java
@@ -7,11 +7,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -20,8 +22,9 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
-public class Lab4 {
+public class Lab9_4 {
 	WebDriver driver;
+	
   @Test()
   public void f() throws InterruptedException {
 	  //title verification
@@ -60,13 +63,23 @@ Click on 'Add to Cart' button
 			driver.findElement(By.name("description")).click();
 			driver.findElement(By.id("button-search")).click();
 			
-			
   }
+  @Parameters("browser")
   @BeforeMethod
-  public void beforeMethod() {
-	  WebDriverManager.chromedriver().setup();
-	  driver=new ChromeDriver();
-	  driver.get("https://tutorialsninja.com/demo/index.php");
+  public void beforeMethod(String browser) {
+		
+	  if(browser.equalsIgnoreCase("chrome")) {
+		  System.out.println("This is test 1");
+		  WebDriverManager.chromedriver().setup();
+		  driver=new ChromeDriver();
+		  driver.get("https://tutorialsninja.com/demo/index.php");
+		  }
+		  else if(browser.equalsIgnoreCase("firefox")) {
+		  System.out.println("This is test 2");
+		  WebDriverManager.firefoxdriver().setup();
+		  driver=new FirefoxDriver();
+		  driver.get("https://tutorialsninja.com/demo/index.php");
+		  }
   }
 
   @AfterMethod
@@ -110,6 +123,5 @@ Click on 'Add to Cart' button
   public void afterSuite() {
 	  System.out.println("After Suite");
   }
-
 
 }
